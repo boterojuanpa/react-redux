b8d9226a64c7fff3f081a784b8a0319f29e08302
Juan Pablo Botero <juan.botero@ceiba.com.co>
2020-03-05 07:01:40 -0500

    se regresa a componente de clase en el contenedor, por problemas identificados a lso hook

--- src/componentes/productos/ContenedorProductos.tsx
+++ src/componentes/productos/ContenedorProductos.tsx
@@ -1,32 +1,52 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { ListaProductos } from './ListaProductos';
 import { AgregarProducto } from './AgregarProducto';
-import { useSelector, useDispatch } from "react-redux";
-import { agregarNuevoProducto, listarProductosAsync, eliminarProducto } from "../../redux/productos/productos.acciones"
-import { EstadoGeneral } from '../../redux/EstadoGeneral';
+import { connect } from "react-redux";
+import { agregarNuevoProducto, eliminarProducto, listarProductosAsync } from "../../redux/productos/productos.acciones"
 import { PaginadorProductos } from './PaginadorProductos';
-
-const ContenedorProductos: React.FC = () => {
-
-
-    const estadoProductos = useSelector((state: EstadoGeneral) => state.productos);
-
-    const dispatch = useDispatch()
-
-    useEffect(() => {
-        dispatch(listarProductosAsync(1));
-    }, [dispatch]);
+import { EstadoGeneral } from '../../redux/EstadoGeneral';
+import { Producto } from './modelo/Producto';
 
 
 
-    return (
-        <React.Fragment>
-            <ListaProductos productos={estadoProductos.productos} onClickEliminarProducto={(producto)=>dispatch(eliminarProducto(producto))}  />
-            <AgregarProducto onClickAgregarProducto={(producto) => dispatch(agregarNuevoProducto(producto))} />
-            <PaginadorProductos cantidadTotalProductos={estadoProductos.cantidadTotalProducto} onClickCambiarPagina={(pagina) => dispatch(listarProductosAsync(pagina))} />
-        </React.Fragment>
-    );
+interface Props {
+    productos: Array<Producto>,
+    listarProductos: (numeroPagina: number) => void
+    agregarNuevoProducto: (productos: Producto) => void,
+    eliminarProducto: (productos: Producto) => void,
+    cantidadTotalProducto: number
 }
 
+class ContenedorProductos extends React.Component<Props, any> {
 
-export default ContenedorProductos;
+
+    componentDidMount() {
+        this.props.listarProductos(1);
+    }
+
+    render() {
+
+        const { productos, cantidadTotalProducto, listarProductos } = this.props;
+
+        return (
+            <React.Fragment>
+                <ListaProductos productos={productos} onClickEliminarProducto={this.props.eliminarProducto} />
+                <AgregarProducto onClickAgregarProducto={this.props.agregarNuevoProducto} />
+                <PaginadorProductos cantidadTotalProductos={cantidadTotalProducto} onClickCambiarPagina={listarProductos} />
+            </React.Fragment>
+        );
+    }
+}
+
+const mapStateToProps = (state: EstadoGeneral) => {
+    return state.productos;
+};
+
+export default connect(
+    mapStateToProps,
+    {
+        listarProductos: listarProductosAsync,
+        agregarNuevoProducto: agregarNuevoProducto,
+        eliminarProducto: eliminarProducto
+    }
+)(ContenedorProductos);
